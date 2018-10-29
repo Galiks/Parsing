609cbd1fde5a0d60201f4af268bafd376be322dd
galiks <turchenkov.pasha@list.ru>
2018-10-29 21:03:36 +0400

    +

--- Parsing/SiteParsing.cs
+++ Parsing/SiteParsing.cs
@@ -10,14 +10,63 @@ namespace Parsing
 {
     class SiteParsing
     {
-        static List<string> names = new List<string>();
-        static List<string> discount = new List<string>();
-        static List<int> pages = new List<int>();
+        private static List<string> names = new List<string>();
+        private static List<string> discount = new List<string>();
+        private static List<int> pages = new List<int>();
+        //private static List<string> pagesOnSite = new List<string>();
+        private int? maxPage;
+
+        public int? MaxPage { get => maxPage; set => maxPage = value; }
+        public static List<string> Names { get => names; set => names = value; }
+        public static List<string> Discount { get => discount; set => discount = value; }
+        public static List<int> Pages { get => pages; set => pages = value; }
+        //public static List<string> PagesOnSite { get => pagesOnSite; set => pagesOnSite = value; }
 
         public void MainMethod()
         {
-            var url = "https://letyshops.com/shops?page=1";
+
+            
             var webGet = new HtmlWeb();
+            MaxPage = MaxPageOnSite(webGet);
+
+            //for (int i = 1; i < MaxPage; i++)
+            //{
+
+                //var url = $"https://letyshops.com/shops?page={i}";
+
+                if (webGet.Load($"https://letyshops.com/shops?page=1") is HtmlDocument document)
+                {
+                    var nodes = document.DocumentNode.CssSelect("div.b-teaser-list");
+
+                    foreach (var item in nodes)
+                    {
+                        foreach (var item2 in item.CssSelect("div.b-teaser__title"))
+                        {
+                            Names.Add(item2.InnerText.CleanInnerText());
+                        }
+                    }
+                    foreach(var item in nodes)
+                    { 
+                        foreach (var item2 in item.CssSelect("span.b-shop-teaser__cash"))
+                        {
+                            Discount.Add(item2.InnerText.CleanInnerText());
+                        }
+                    }
+
+                    Console.WriteLine($"{Names.Count} : {Discount.Count}");
+
+                    for (int j = 0; j <= Names.Count; j++)
+                    {
+                        Console.WriteLine($"{Names[j]} : {Discount[j]}");
+                    }
+                } 
+            //}
+        }
+
+        private static int? MaxPageOnSite(HtmlWeb webGet)
+        {
+            var url = "https://letyshops.com/shops?page=1";
+
             if (webGet.Load(url) is HtmlDocument document)
             {
                 var maxPage = document.DocumentNode.CssSelect("ul.b-pagination").CssSelect("li.b-pagination__item").CssSelect("a.b-pagination__link");
@@ -26,32 +75,15 @@ namespace Parsing
                 {
                     if (Int32.TryParse(item.InnerText, out int result))
                     {
-                        pages.Add(result);
+                        Pages.Add(result);
                     }
                 }
 
-                Console.WriteLine(pages.Max());
+                return Pages.Max();
 
-                var nodes = document.DocumentNode.CssSelect("div.b-teaser-list");
-
-                foreach (var item in nodes)
-                {
-                    foreach (var item2 in item.CssSelect("div.b-teaser__title"))
-                    {
-                        names.Add(item2.InnerText.CleanInnerText());
-                    }
-
-                    foreach (var item2 in item.CssSelect("span.b-shop-teaser__cash"))
-                    {
-                        discount.Add(item2.InnerText.CleanInnerText());
-                    }
-                }
-
-                for (int i = 0; i < discount.Count; i++)
-                {
-                    Console.WriteLine($"{names[i]} : {discount[i]}");
-                }
             }
+
+            return null;
         }
     }
 }
